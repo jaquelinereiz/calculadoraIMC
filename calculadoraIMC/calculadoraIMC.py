191f8147565ebc3bec1898e0547d3c17466db178
Jaqueline Reis <91039376+jaquelinereiz@users.noreply.github.com>
2023-10-08 19:17:17 -0300

    add projeto
    adicionando projeto ao repositório

---
+++ calculadoraIMC/calculadoraIMC.py
@@ -0,0 +1,27 @@
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    return imc
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif 18.5 <= imc < 24.9:
+        return "Peso normal"
+    elif 25 <= imc < 29.9:
+        return "Sobrepeso"
+    elif 30 <= imc < 34.9:
+        return "Obesidade Grau 1"
+    elif 35 <= imc < 39.9:
+        return "Obesidade Grau 2"
+    else:
+        return "Obesidade Grau 3"
+
+imc = calcular_imc(peso, altura)
+
+classificacao = classificar_imc(imc)
+
+peso = float(input("Digite seu peso em kg: "))
+altura = float(input("Digite sua altura em metros: "))
+
+print(f"Seu IMC é {imc:.2f}")
+print(f"Classificação: {classificacao}")
